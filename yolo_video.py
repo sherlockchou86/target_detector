dd9b6773c5f948ffc8808a36390a7b693b83db06
sherlockchou86 <zhouzhi9@foxmail.com>
2018-06-12 19:42:42 +0800

    add base code

---
+++ yolo_video.py
@@ -0,0 +1,15 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: $ python {0} [video_path, [, output_path]]", sys.argv[0])
+    exit()
+
+from yolo import YOLO
+from yolo import detect_video
+
+if __name__ == '__main__':
+    video_path = sys.argv[1]
+    output_path = ""
+    if len(sys.argv) > 2:
+        output_path = sys.argv[2]
+    detect_video(YOLO(), video_path, output_path)
